e6776cb43f7291918a88fbdaf87d7c4914d12284
Edoardo Pasca <edo@edosil.net>
2018-11-03 10:08:44 +0000

    added logging and argument to pass directory to read

--- organise.py
+++ organise.py
@@ -3,6 +3,7 @@ import PIL.ExifTags
 from datetime import datetime
 import os
 import shutil
+import sys
 
 
 
@@ -22,8 +23,8 @@ def organise_picture(filename, destination):
     dt = datetime.strptime(exif['DateTimeDigitized'],
                                  '%Y:%m:%d %H:%M:%S') 
     
-    print ("should create directory {0}/{1}".format(
-        dt.year,dt.month))
+    #print ("should create directory {0}/{1}".format(
+    #    dt.year,dt.month))
     
     copyto = os.path.join(destination, str(dt.year) , str(dt.month) , 
             os.path.basename(current_image))
@@ -37,23 +38,35 @@ def organise_picture(filename, destination):
         #print (mdir, " exists : ", oe )
         pass
     print ("copy to " , copyto)
-    #shutil.copy2(current_image, copyto)
+    shutil.copy2(current_image, copyto)
 
 
-#base_directory = os.path.join("/mnt","share","Pictures","macchinanovaedo")
-base_directory = os.path.abspath("/mnt/share/Pictures/nexus-s/2012-2013/")
-all_pics = os.listdir(base_directory)
+base_directory = os.path.join("/mnt","share","Pictures","tutte")
+iarg = sys.argv[1]
+if iarg == "":
+    orig_dir = os.path.abspath("/mnt/share/Pictures/nexus-s/2012-2013/")
+else:
+    orig_dir = os.path.abspath(sys.argv[1])
+
+all_pics = os.listdir(orig_dir)
 success = []
 fail = []
 for i,el in enumerate(all_pics):
     print ("Progress {0}/{1} {2}".format(i,len(all_pics),el))
     try:
-        current_picture = os.path.join(base_directory, el)
-        organise_picture(current_picture, 
-            base_directory)
+        current_picture = os.path.join(orig_dir, el)
+        organise_picture(current_picture, base_directory)
         success.append(current_picture)
     except OSError as oe:
         fail.append(current_picture)
-    break
 
+
+with open("organise.log","w") as f:
+    f.write("Organising pictures \n  from {0}\n  to   {1}\n"
+            .format(orig_dir, base_directory))
+    f.write("Success: {}/{}\n".format(len(success),len(all_pics)))
+    f.write("Fail   : {}/{}\n".format(len(fail),len(all_pics)))
+    f.write("Failed file list:\n")
+    for failed in fail:
+        f.write("{}\n".format(failed))
 
